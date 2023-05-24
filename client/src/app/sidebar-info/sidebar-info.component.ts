653c1a7be71f83940356ae642f2b5ab199a4f89b
Kanturrya <a.horion@orange.fr>
2023-05-24 12:48:37 +0200

    Modif. gestion erreur

--- client/src/app/sidebar-info/sidebar-info.component.ts
+++ client/src/app/sidebar-info/sidebar-info.component.ts
@@ -13,12 +13,29 @@ export class SidebarInfoComponent implements OnInit{
 
   constructor(private http: HttpClient) { 
     this.logs = [];
-    setInterval(() => this.readLogs(), 5000);
+    this.readLogs();
+    setInterval(() => this.readLog(), 5000);
    }
 
   ngOnInit(): void { }
   
   readLogs(){
+    this.http.get(this.url+'/getlogs', { responseType: 'text' }).subscribe((data: string) => {
+      const parsedData: string[] = JSON.parse(data) as string[];
+      
+      parsedData.forEach((element,index) => {
+        if(element=="") parsedData.splice(index,1);
+      });
+      this.logs = this.logs.concat(parsedData);
+
+      //console.log(this.logs);
+      
+    }, (error) => {
+      console.error('Erreur lors de la lecture du fichier :', error);
+    });
+  }
+
+  readLog(){
     this.http.get(this.url+'/getlog', { responseType: 'text' }).subscribe((data: string) => {
       const parsedData: string[] = JSON.parse(data) as string[];
       
